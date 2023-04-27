b34b32272b0021bd1f183edf6818d5806fe6a7e7
Rafael perez guzman <rafisic@misena.edu.co>
2023-04-26 22:46:26 -0500

    Add files via upload

---
+++ css/script/miescript.js
@@ -0,0 +1,92 @@
+
+
+
+   
+
+
+    
+
+
+
+    
+      
+
+       
+       function mostrarlista() {
+       // var identificador=document.getElementById("hide").style.display="block";
+        //document.getElementById("hide").style.height="150px";
+       
+       
+      
+       
+        
+            document.getElementById("encendido").style.display="inline-block";
+            
+         
+           document.getElementById("oculta").style.display="none";
+            document.getElementById("muestra").style.display="inline-block";
+          
+           
+        
+           
+            
+    
+    }
+
+       
+    
+ 
+
+
+   
+
+
+
+
+    function ocultarmenu () {
+    
+        document.getElementById("encendido").style.display="none";
+        document.getElementById("oculta").style.display="inline-block";
+        document.getElementById("muestra").style.display="none";
+       
+
+    }
+    
+        
+        
+        
+   /*function obtener() {
+        var w = window.outerWidth;
+        var h = window.outerHeight;
+        document.getElementById("bodi").w;
+        alert(w);
+
+
+        
+
+    }*/
+    
+   
+      
+       function restaura(){
+        document.getElementById("encendido").style.display="inline-block";
+       }
+    
+   
+    
+       /*            
+ window.addEventListener("scroll", (event)=>{
+            var scroll=this.scrollY;
+            console.log(scroll);
+            if(scroll==350){
+                document.getElementById("comentario").style.color="yellow";
+            }
+        })*/
+
+        function seccion(){
+
+var scroll=scrollY; console.log(scroll);
+
+
+    }
+    
